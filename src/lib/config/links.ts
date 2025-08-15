366812617c9d079da5e984e41d33b6024c4f4dab
CarboxyDev <carboxy17@gmail.com>
2025-08-15 19:29:30 +0530

    Remove blogs page + Update blogs button to redirect to my substack

--- src/lib/config/links.ts
+++ src/lib/config/links.ts
@@ -1,5 +1,5 @@
 export const LINKS = {
-  blog: '/blog',
+  blog: 'https://carboxy.substack.com/',
   github: 'https://github.com/CarboxyDev',
   twitter: 'https://x.com/CarboxyDev',
   email: 'mailto:carboxy17@gmail.com',
